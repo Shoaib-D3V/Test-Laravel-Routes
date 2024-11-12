a7561a5cda615f5696ca249d7bba35f1d1ff587e
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-12 13:26:42 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,9 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
 
+    Route::prefix('v1')->group(function () {
+    Route::resource('/tasks', TaskController::class);
+});
+
+
 });
